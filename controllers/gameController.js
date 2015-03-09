7a856c0e9478ac6db4bf850111f1a67d6c7c6298
Monty Rohde <nathanarohde@gmail.com>
2015-03-09 16:39:23 -0700

    In the process of adding a dice rolling feature.  End of day commit.

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,4 +1,10 @@
 cantStop.controller('gameCtrl', function gameCtrl($scope, playersFactory, utilitiesFactory) {
   $scope.players = playersFactory.players;
   $scope.playersFactory = playersFactory;
+
+  roll = function() {
+    dice1 = Math.ceil(6*Math.random());
+    return dice1;
+  };
+
 });
