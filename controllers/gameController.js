6e268227e473cfd6e452f64391e1480dd948a5f2
Monty Rohde <nathanarohde@gmail.com>
2015-03-10 12:08:30 -0700

    After dice are rolled computer calculates and displays all pair values of the dice.

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -2,9 +2,18 @@ cantStop.controller('gameCtrl', function gameCtrl($scope, playersFactory, utilit
   $scope.players = playersFactory.players;
   $scope.playersFactory = playersFactory;
   $scope.dice = {die1: null, die2: null, die3: null, die4: null}
+  $scope.pairs = [ ]
 
   $scope.diceRoll = function() {
-    $scope.dice = {die1: Math.ceil(6*Math.random()), die2: Math.ceil(6*Math.random()), die3: Math.ceil(6*Math.random()), die4: Math.ceil(6*Math.random())}
+    $scope.pairs = [ ]
+    $scope.dice = {die1: Math.ceil(6*Math.random()), die2: Math.ceil(6*Math.random()), die3: Math.ceil(6*Math.random()), die4: Math.ceil(6*Math.random())};
+    $scope.rollPairs();
+  };
+
+  $scope.rollPairs = function() {
+    $scope.pairs.push({pair1: ($scope.dice.die1 + $scope.dice.die2), pair2: ($scope.dice.die3 + $scope.dice.die4)});
+    $scope.pairs.push({pair1: ($scope.dice.die1 + $scope.dice.die3), pair2: ($scope.dice.die2 + $scope.dice.die4)});
+    $scope.pairs.push({pair1: ($scope.dice.die1 + $scope.dice.die4), pair2: ($scope.dice.die2 + $scope.dice.die3)});
   };
 
 });
