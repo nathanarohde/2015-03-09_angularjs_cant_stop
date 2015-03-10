7b7f11b2aa76cd4a04bce436ebe122f0753b334a
Monty Rohde <nathanarohde@gmail.com>
2015-03-10 16:36:28 -0700

    Program stores die roll in an array instead of as an object.  The intent of this change is to make the validation checks easier to write.

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,36 +1,46 @@
 cantStop.controller('gameCtrl', function gameCtrl($scope, playersFactory, utilitiesFactory) {
   $scope.players = playersFactory.players;
   $scope.playersFactory = playersFactory;
-  $scope.dice = {die1: null, die2: null, die3: null, die4: null};
+  $scope.dice = [ ];
+  // $scope.dice = {die1: null, die2: null, die3: null, die4: null};
   $scope.pairs = [ ];
   $scope.currentPlayer = utilitiesFactory.findById(playersFactory.players, 1);
   $scope.currentDiceRollChoices = [ ]
 
   $scope.diceRoll = function() {
+    $scope.dice = [ ]
     $scope.pairs = [ ]
-    $scope.dice = {die1: Math.ceil(6*Math.random()), die2: Math.ceil(6*Math.random()), die3: Math.ceil(6*Math.random()), die4: Math.ceil(6*Math.random())};
+    for(i=0; i< 4; i++) {
+      $scope.dice.push(Math.ceil(6*Math.random()));
+    }
+    // $scope.dice = {die1: Math.ceil(6*Math.random()), die2: Math.ceil(6*Math.random()), die3: Math.ceil(6*Math.random()), die4: Math.ceil(6*Math.random())};
     $scope.rollPairs();
   };
 
   $scope.rollPairs = function() {
-    $scope.pairs.push({pair1: ($scope.dice.die1 + $scope.dice.die2), pair2: ($scope.dice.die3 + $scope.dice.die4)});
-    $scope.pairs.push({pair1: ($scope.dice.die1 + $scope.dice.die3), pair2: ($scope.dice.die2 + $scope.dice.die4)});
-    $scope.pairs.push({pair1: ($scope.dice.die1 + $scope.dice.die4), pair2: ($scope.dice.die2 + $scope.dice.die3)});
+
+    $scope.pairs.push({pair1: ($scope.dice[0] + $scope.dice[1]), pair2: ($scope.dice[2] + $scope.dice[3])});
+    $scope.pairs.push({pair1: ($scope.dice[0] + $scope.dice[2]), pair2: ($scope.dice[1] + $scope.dice[3])});
+    $scope.pairs.push({pair1: ($scope.dice[0] + $scope.dice[3]), pair2: ($scope.dice[1] + $scope.dice[2])});
+
+    // $scope.pairs.push({pair1: ($scope.dice.die1 + $scope.dice.die2), pair2: ($scope.dice.die3 + $scope.dice.die4)});
+    // $scope.pairs.push({pair1: ($scope.dice.die1 + $scope.dice.die3), pair2: ($scope.dice.die2 + $scope.dice.die4)});
+    // $scope.pairs.push({pair1: ($scope.dice.die1 + $scope.dice.die4), pair2: ($scope.dice.die2 + $scope.dice.die3)});
   };
 
   $scope.chooseDiceRolls = function(combo) {
-debugger;
+
     if ($scope.currentDiceRollChoices.length <= 3) {
       $scope.currentDiceRollChoices.push(combo.pair1);
       $scope.currentDiceRollChoices.push(combo.pair2);
       $scope.diceRoll();
     } else {
-      $scope.currentDiceRollChoices = [ ]
       $scope.endOfTurn();
     }
   };
 
   $scope.endOfTurn = function() {
+    $scope.currentDiceRollChoices = [ ]
     if ($scope.currentPlayer.id < $scope.players.length) {
         $scope.currentPlayer = utilitiesFactory.findById(playersFactory.players, ($scope.currentPlayer.id + 1))
     } else {
