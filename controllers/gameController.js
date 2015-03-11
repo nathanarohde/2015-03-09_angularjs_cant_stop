494275a49eb41941709e1a025bf3d02eb58edaa8
Monty Rohde <nathanarohde@gmail.com>
2015-03-11 13:39:04 -0700

    Keep a rolling sum the number of times a dice has been rolled.  Will not allow the player to enter more than four dice and does not accept hash keys as a dice roll choice.  Perviously hash keys were being entered as part of the loop.

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -2,10 +2,11 @@ cantStop.controller('gameCtrl', function gameCtrl($scope, playersFactory, utilit
   $scope.players = playersFactory.players;
   $scope.playersFactory = playersFactory;
   $scope.dice = [ ];
-  // $scope.dice = {die1: null, die2: null, die3: null, die4: null};
   $scope.pairs = [ ];
   $scope.currentPlayer = utilitiesFactory.findById(playersFactory.players, 1);
   $scope.currentDiceRollChoices = [ ]
+  $scope.currentRollScore = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+
 
   $scope.diceRoll = function() {
     $scope.dice = [ ]
@@ -13,7 +14,6 @@ cantStop.controller('gameCtrl', function gameCtrl($scope, playersFactory, utilit
     for(i=0; i< 4; i++) {
       $scope.dice.push(Math.ceil(6*Math.random()));
     }
-    // $scope.dice = {die1: Math.ceil(6*Math.random()), die2: Math.ceil(6*Math.random()), die3: Math.ceil(6*Math.random()), die4: Math.ceil(6*Math.random())};
     $scope.rollPairs();
   };
 
@@ -22,29 +22,68 @@ cantStop.controller('gameCtrl', function gameCtrl($scope, playersFactory, utilit
     $scope.pairs.push({pair1: ($scope.dice[0] + $scope.dice[1]), pair2: ($scope.dice[2] + $scope.dice[3])});
     $scope.pairs.push({pair1: ($scope.dice[0] + $scope.dice[2]), pair2: ($scope.dice[1] + $scope.dice[3])});
     $scope.pairs.push({pair1: ($scope.dice[0] + $scope.dice[3]), pair2: ($scope.dice[1] + $scope.dice[2])});
-
-    // $scope.pairs.push({pair1: ($scope.dice.die1 + $scope.dice.die2), pair2: ($scope.dice.die3 + $scope.dice.die4)});
-    // $scope.pairs.push({pair1: ($scope.dice.die1 + $scope.dice.die3), pair2: ($scope.dice.die2 + $scope.dice.die4)});
-    // $scope.pairs.push({pair1: ($scope.dice.die1 + $scope.dice.die4), pair2: ($scope.dice.die2 + $scope.dice.die3)});
   };
+
+//   $scope.isValidRoll = function() {
+//     if ($scope.currentDiceRollChoices.length < 3) {
+//       return true;
+//     } else if ($scope.compareChoicetoRoll() ) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   };
+//
+//   $scope.compareChoicetoRoll = function() {
+//     for (number in $scope.currentDiceRollChoices) {
+//       for (pair in $scope.pairs) {
+//         if ($scope.currentDiceRollChoices(number) === $scope.pairs(pair)){
+//
+// debugger;
+//           return true;
+//         }
+//       }
+//
+//     }
+//     return false;
+//   };
 
   $scope.chooseDiceRolls = function(combo) {
 
-    if ($scope.currentDiceRollChoices.length <= 3) {
-      $scope.currentDiceRollChoices.push(combo.pair1);
-      $scope.currentDiceRollChoices.push(combo.pair2);
-      $scope.diceRoll();
-    } else {
-      $scope.endOfTurn();
-    }
+    // if ($scope.isValidRoll()) {
+    // combo.forEach(function(pair) {
+    for(var pair in combo) {
+      if (typeof combo[pair] == 'number') {
+        var number = combo[pair]
+        if ($scope.currentDiceRollChoices.indexOf(number) != -1){
+          $scope.currentRollScore[number - 2] += 1;
+          $scope.diceRoll();
+        } else if ($scope.currentDiceRollChoices.length < 3) {
+          $scope.currentDiceRollChoices.push(number);
+          $scope.currentRollScore[number - 2] += 1;
+          $scope.diceRoll();
+        } else {
+        $scope.endOfTurn();
+        }
+      }
+    };
   };
+
+    // if ($scope.currentDiceRollChoices.length < 3) {
+    //   if (combo.pair1)
+    //   $scope.currentDiceRollChoices.push(combo.pair1);
+    //   $scope.currentDiceRollChoices.push(combo.pair2);
+    //   $scope.diceRoll();
 
   $scope.endOfTurn = function() {
     $scope.currentDiceRollChoices = [ ]
     if ($scope.currentPlayer.id < $scope.players.length) {
         $scope.currentPlayer = utilitiesFactory.findById(playersFactory.players, ($scope.currentPlayer.id + 1))
+        $scope.currentRollScore = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+
     } else {
         $scope.currentPlayer = utilitiesFactory.findById(playersFactory.players, 1)
+        $scope.currentRollScore = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
     }
   };
 
